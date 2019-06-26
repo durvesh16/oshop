dc26192dd362b41440234e0c7f83a7ee8059b52c
viraj <viraj.dani@isynergysys.com>
2019-06-26 16:50:06 +0530

    oshop first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCnvXeHb2VuHi05QZ_RdWpoziWxlrBjNYQ',
+    authDomain: 'oshop-e3e4d.firebaseapp.com',
+    databaseURL: 'https://oshop-e3e4d.firebaseio.com',
+    projectId: 'oshop-e3e4d',
+    storageBucket: '',
+    messagingSenderId: '1088989076724',
+    appId: '1:1088989076724:web:5fc4a75ad65c0f13'
+  }
 };
 
 /*
